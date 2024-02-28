3574050a5240340c4f1f97421e2ff6bcbc40f40e
Andy <avp365@yandex.ru>
2024-02-28 10:33:00 +0000

    правки

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,78 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+
+	start := time.Now()
+
+	n := 2
+	summ := 0
+
+	data := calc(n, n)
+
+	for _, valueInt := range data {
+		summ += valueInt
+	}
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+
+	fmt.Println(summ, elapsed)
+
+}
+
+func calc(n int, ni int) []int {
+
+	data := make([][]int, 9+1)
+	res := make([]int, ni*9+1)
+
+	for i := range data {
+		data[i] = make([]int, ni*9+1)
+	}
+
+	if ni > 1 {
+		res = calc(n, ni-1)
+
+	}
+
+	if ni == 1 {
+		for s := 0; s <= ni*9; s++ {
+
+			for i := 0; i <= 9; i++ {
+
+				if s == i {
+					data[s][i] = 1
+				}
+				continue
+			}
+
+		}
+	}
+
+	if ni > 1 {
+		fmt.Println(res)
+
+		for i := 0; i <= 9; i++ {
+			for s, v := range res {
+				data[i][i+s] = v
+			}
+		}
+
+	}
+	res = make([]int, ni*9+1)
+	for s := 0; s <= ni*9; s++ {
+		for i := 0; i <= 9; i++ {
+
+			res[s] = res[s] + data[i][s]
+
+		}
+		res[s] = res[s] * res[s]
+	}
+
+	return res
 
 }
