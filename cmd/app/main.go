91f54846d3a1fc2054e6df0c22fa3424d9558fd9
Andy <avp365@yandex.ru>
2024-03-01 16:32:12 +0000

    правки

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"math"
+	"time"
+
 	"github.com/avp365/arch-pat/internal/pkg/task"
 )
 
 func main() {
-	task.Task(3)
+	a := 3.0
+	n := 5.0
+	task.T1(int(a), int(n))
+	start := time.Now()
+
+	answer := math.Pow(a, n)
+
+	end := time.Now()
+	elapsed := end.Sub(start)
+	fmt.Println(answer, elapsed)
 }
